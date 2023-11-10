5bdc4b735ae58eb9df03f44786045ee8f76d5ddc
Doppelgangers <6666genocide666@mail.ru>
2023-11-10 18:18:44 +0300

    Валидация данных с Pydantic

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,11 @@
+import datetime
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
 import uvicorn
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
@@ -10,12 +16,37 @@ fake_users = [
 ]
 
 
-@app.get("/user/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    type_degree: DegreeType
+    received_date: datetime.datetime
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/user/{user_id}", response_model=User)
 def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
+    return [user for user in fake_users if user.get("id") == user_id][0]
 
 
-fake_trades = [
+@app.post("/user/{user_id}")
+def change_name(user_id: int, name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
+    current_user["name"] = name
+    return {"status": 200, "new_name": name}
+
+
+fake_trades: List = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "sell", "price": 123, "amount": 2.12},
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
 ]
@@ -26,11 +57,19 @@ def get_trades(limit: int = 10, offset: int = 0):
     return fake_trades[offset:][:limit]
 
 
-@app.post("/user/{user_id}")
-def change_name(user_id: int, name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
-    current_user["name"] = name
-    return {"status": 200, "new_name": name}
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    sell: float
+    price: float = Field(ge=0)
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 if __name__ == '__main__':
